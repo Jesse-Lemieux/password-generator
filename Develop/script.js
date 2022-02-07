c74431f759e5beecb88d1a02d7567e6dfed5f2f9
JESSE <dtrsjesse121@gmail.com>
2022-02-07 16:10:37 -0500

    Added base vars and connected generate button

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,16 @@
-// Assignment code here
+
+// Password generator : chooses chacrters from a length of 8-100 to randomly generate password
+
+//Assignment Code
+document.querySelector("#generate").addEventListener("click", writePassword);
+
+// Character Arrays
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
@@ -10,8 +18,4 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
