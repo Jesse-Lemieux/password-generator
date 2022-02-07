563301fe5905c4b3c5b9e65b51fd2102168c9829
JESSE <dtrsjesse121@gmail.com>
2022-02-07 16:32:37 -0500

    add rest of window prompts for customization

--- README.md
+++ README.md
@@ -4,8 +4,10 @@
 -Created base vars and provided a path to the "generate" button.
 -Created the first prompt, length, and made another variable section for declared variables.
 -Made the confirmation of length forced to be 8-100 characters or it loops back.
+-Added the rest or the declared variables.
+-Added the rest of the window prompts.
+-Optimized password choices to concatenate with password.
 -
-
 
 
 
--- script.js
+++ script.js
@@ -12,6 +12,10 @@ var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M
 
 // Declared Variables 
 var confirmLength = "";
+var confirmSpecialCharacter;
+var confirmNumericCharacter;
+var confirmUpperCase;
+var confirmLowerCase;
 
 // Prompt to confirm how many characters the user would like in their password
 function generatePassword() {
@@ -25,6 +29,40 @@ function generatePassword() {
 
     // Alert informing user of password length chosen 
     alert(`Your password will have ${confirmLength} characters`);
+
+        // Confirm characters used 
+        var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
+        var confirmNumericCharacter = confirm("Click OK to confirm if you would like to include numeric characters");    
+        var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters");
+        var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters");
+          // If none are chosen, loop back
+          while(confirmUpperCase === false && confirmLowerCase === false && confirmSpecialCharacter === false && confirmNumericCharacter === false) {
+            alert("You must choose at least one parameter");
+            var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
+            var confirmNumericCharacter = confirm("Click OK to confirm if you would like to include numeric characters");    
+            var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters");
+            var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters");   
+        } 
+    
+          var passwordCharacters = []
+          
+        if (confirmSpecialCharacter) {
+          passwordCharacters = passwordCharacters.concat(specialChar)
+        }
+    
+        if (confirmNumericCharacter) {
+          passwordCharacters = passwordCharacters.concat(number)
+        }
+          
+        if (confirmLowerCase) {
+          passwordCharacters = passwordCharacters.concat(alphaLower)
+        }
+    
+        if (confirmUpperCase) {
+          passwordCharacters = passwordCharacters.concat(alphaUpper)
+        }
+    
+          console.log(passwordCharacters)
 
 
 // Write password to the #password input
